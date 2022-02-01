2ec594f7bf976c0467e74e8d18f840b2bbb4f73b
Pravin Suresh <selfstudy210619@gmail.com>
2022-02-01 22:03:23 +0530

    Added sample data for stocks, header-row and footer-row

--- src/data.js
+++ src/data.js
@@ -4,3 +4,37 @@ export const dropDownData = [
     {id:3, value:'Small Case'},
     {id:4, value:'Mutual Funds'}
 ];
+
+
+export const StocksData = [
+    {
+        Instrument:"Adani Power",
+        category: "kite",
+        Qty: 83,
+        AverageCost: 108.25,
+        LTP: 108,
+    },
+    {
+        Instrument:"Mindtree",
+        category: "kite",
+        Qty: 3,
+        AverageCost: 3755,
+        LTP: 3983.7,
+    }
+]
+
+export const HeaderRows = [
+    "Instrument",
+    "Qty",
+    "Avg. cost",
+    "LTP",
+    "Cur. val",
+    "P&L",
+    "Net Change"
+]
+
+export const FooterRows = [
+    "Total Investment",
+    "Current Value",
+    "Total P&L"
+]
