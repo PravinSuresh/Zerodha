8a7c61d8a87e46e2c219f497eb6965a72db5157c
Pravin Suresh <selfstudy210619@gmail.com>
2022-02-06 23:12:20 +0530

    combined two commits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Table from './components/Table/Table';
+import {StocksData, HeaderRows, FooterRows} from './data';
 
 function App() {
   return (
     <div>
       <Header count={12} />
+      <Table headerData={HeaderRows} mainData={StocksData} footerData={FooterRows} />
     </div>
   );
 }
